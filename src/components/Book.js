a0a9d2c0f7f0ae8481b434c054e4076913cbb209
markobozic346 <55995892+markobozic346@users.noreply.github.com>
2021-03-17 08:22:42 +0100

    added basic css for book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
-import './Book.css';
+import "./Book.css";
 const Book = () => {
   const { id } = useParams();
 
@@ -16,36 +16,37 @@ const Book = () => {
   }, []);
   return (
     <div className="book">
-        <h1>{data.title}</h1>
-        <h3>{data.desc}</h3>
-      <img src={data.image} alt="book image" />
+      <h1 className="title">{data.title}</h1>
+      <h3 className='desc'>{data.desc}</h3>
 
-      <table>
-          <tbody>
-        <tr>
-          <td>Price</td>
-          <td>{data.price}</td>
-        </tr>
-        <tr>
-          <td>Authors</td>
-          <td>{data.authors}</td>
-        </tr>
-        <tr>
-          <td>Publisher</td>
-          <td>{data.publisher}</td>
-        </tr>
-        <tr>
-          <td>Pages</td>
-          <td>{data.pages}</td>
-        </tr>
-        <tr>
-          <td>Language</td>
-          <td>{data.language}</td>
-        </tr>
-        <tr>
-          <td>Year</td>
-          <td>{data.year}</td>
-        </tr>
+      <img className='image'src={data.image} alt="book image" />
+
+      <table >
+        <tbody>
+          <tr>
+            <td>Price</td>
+            <td>{data.price}</td>
+          </tr>
+          <tr>
+            <td>Authors</td>
+            <td>{data.authors}</td>
+          </tr>
+          <tr>
+            <td>Publisher</td>
+            <td>{data.publisher}</td>
+          </tr>
+          <tr>
+            <td>Pages</td>
+            <td>{data.pages}</td>
+          </tr>
+          <tr>
+            <td>Language</td>
+            <td>{data.language}</td>
+          </tr>
+          <tr>
+            <td>Year</td>
+            <td>{data.year}</td>
+          </tr>
         </tbody>
       </table>
       {console.log(data)}
